4b23aa9a12e71f69d1a195e25b7efd42f570ad39
martti <info@martti.be>
2015-09-13 13:00:31 +0200

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+### phpBB 3.1 PhpBB Extension - marttiphpbb Parameter Template Variables
+
+
+
+#### Quick Install
+
+You can install this on the latest release of phpBB 3.1 by following the steps below:
+
+* Create `marttiphpbb/paramtempvars` in the `ext` directory.
+* Download and unpack the repository into `ext/marttiphpbb/paramtempvars`
+* Enable `Parameter Template Variables` in the ACP at `Customise -> Manage extensions`.
+
+#### Uninstall
+
+* Disable `Parameter Template Variables` in the ACP at `Customise -> Extension Management -> Extensions`.
+* To permanently uninstall, click `Delete Data`
+
+
+#### Support
+
+* **Important: Only official release versions validated by the phpBB Extensions Team should be installed on a live forum. Pre-release (beta, RC) versions downloaded from this repository are only to be used for testing on offline/development forums and are not officially supported.**
+* Report bugs and other issues to the [Issue Tracker](https://github.com/marttiphpbb/phpbb-ext-paramtempvars/issues).
+
+#### License
+
+[GPL-2.0](license.txt)
---
+++ event/listener.php
@@ -0,0 +1,68 @@
+<?php
+/**
+* phpBB Extension - marttiphpbb paramtempvars
+* @copyright (c) 2014 marttiphpbb <info@martti.be>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace marttiphpbb\paramtempvars\event;
+
+use phpbb\template\twig\twig as template;
+use phpbb\user;
+
+use marttiphpbb\paramtempvars\model\paramtempvars_directory;
+
+/**
+* @ignore
+*/
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+* Event listener
+*/
+class listener implements EventSubscriberInterface
+{
+	/* @var template */
+	protected $template;
+
+	/* @var user */
+	protected $user;
+
+	/**
+	 * @param template $template
+	 * @param user $user
+	*/
+	public function __construct(
+		template $template,
+		user $user
+	)
+	{
+		$this->template = $template;
+		$this->user = $user;
+	}
+
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.page_footer'		=> 'core_page_footer',
+		);
+	}
+
+	public function core_page_footer($event)
+	{
+		$query_string = $this->user->page['query_string'];
+
+		$params = $template_vars = array();
+		parse_str($query_string, $params);
+
+		foreach ($params as $name => $value)
+		{
+			$template_vars['CUSTOMCODE_PARAM_' . strtoupper($name)] = $value;
+		}
+
+		if (sizeof($template_vars))
+		{
+			$this->template->assign_vars($template_vars);
+		}
+	}
+}
